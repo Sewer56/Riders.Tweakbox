42a1f2dfa61ed197c0785d769a7c3796745a89f3
Sewer56 <seweratwork@gmail.com>
2021-03-03 19:02:52 +0000

    Feature: Try Blocking DLL injection for DLLs that cause issues.

--- Riders.Tweakbox/DllNotifier.cs
+++ Riders.Tweakbox/DllNotifier.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Runtime.InteropServices;
 using Reloaded.Hooks.Definitions;
 
 // ReSharper disable once RedundantUsingDirective
@@ -25,6 +26,14 @@ namespace Riders.Tweakbox
             new NotifyEntry("RTSSHooks.dll", "RivaTuner Statistics Server", $"Most commonly ships with MSI Afterburner. Does not use trampoline for function hooks. ({ErrNoTrampoline})"),
         };
 
+        /// <summary>
+        /// List of modules to warn against.
+        /// </summary>
+        public readonly NotifyEntry[] BlockList = new NotifyEntry[] 
+        {
+            new NotifyEntry("speedhack-i386.dll", "Cheat Engine Speed Hack", $"Please don't use it, it makes things really weird in Netplay. (Besides you'd be lame anyway)"),
+        };
+
         private IHook<LdrLoadDll> _ldrLoadDllHook;
 
         public unsafe DllNotifier(IReloadedHooks hooks)
@@ -33,16 +42,35 @@ namespace Riders.Tweakbox
             // We are ensuring that all DLLs used by logging are activated before the hood.
             Log.WriteLine($"[{nameof(DllNotifier)}] Initialising.");
 
-            var ntdll = PInvoke.LoadLibrary("ntdll.dll");
-            var ldrLoadDll = hooks.CreateFunction<LdrLoadDll>((long)Native.GetProcAddress(ntdll, nameof(LdrLoadDll)));
+            var ntdll    = PInvoke.LoadLibrary("ntdll.dll");
+            var ldrLoadDll  = hooks.CreateFunction<LdrLoadDll>((long)Native.GetProcAddress(ntdll, nameof(LdrLoadDll)));
+
             _ldrLoadDllHook = ldrLoadDll.Hook(LdrLoadDllImpl).Activate();
         }
 
-        private unsafe int LdrLoadDllImpl(int searchPath, uint flags, Native.UNICODE_STRING* modulefilename, out IntPtr handle)
+        private unsafe uint LdrLoadDllImpl(int searchPath, uint flags, Native.UNICODE_STRING* modulefilename, out IntPtr handle)
         {
             var moduleName     = modulefilename->ToString();
             var moduleFileName = Path.GetFileName(moduleName);
-            var index          = WarningList.IndexOf(x => x.DllName.Equals(moduleFileName, StringComparison.OrdinalIgnoreCase));
+            if (TestDll(moduleFileName))
+            {
+                PInvoke.SetLastError(0x4); // ACCESS DENIED
+                handle = IntPtr.Zero;
+                return 0xC0000135; // "DLL NOT FOUND"
+            }
+
+            return _ldrLoadDllHook.OriginalFunction(searchPath, flags, modulefilename, out handle);
+        }
+
+        /// <summary>
+        /// Tests a given DLL and presents any warnings as needed.
+        /// </summary>
+        /// <param name="dllName">The name of the DLL to be tested.</param>
+        /// <returns>True if the dll is to be blocked, else false.</returns>
+        private bool TestDll(string dllName)
+        {
+            // Check Warninng List
+            var index = IndexOfDll(WarningList, dllName);
             if (index != -1)
             {
                 var warning = WarningList[index];
@@ -53,7 +81,30 @@ namespace Riders.Tweakbox
                 Log.WriteLine($"If Tweakbox crashes consider disabling this program or finding a way to delay its injection etc.");
             }
 
-            return _ldrLoadDllHook.OriginalFunction(searchPath, flags, modulefilename, out handle);
+            // Check Block List
+            index = IndexOfDll(BlockList, dllName);
+            if (index != -1)
+            {
+                var block = BlockList[index];
+                Log.WriteLine($"[ERROR!!] Blocked library is being loaded.");
+                Log.WriteLine($"DLL Name: {block.DllName}");
+                Log.WriteLine($"Name: {block.Name}");
+                Log.WriteLine($"Reason: {block.Reason}");
+                return true;
+            }
+
+            return false;
+        }
+
+        private int IndexOfDll(NotifyEntry[] source, string dllName)
+        {
+            for (int x = 0; x < source.Length; x++)
+            {
+                if (source[x].DllName.Equals(dllName, StringComparison.OrdinalIgnoreCase))
+                    return x;
+            }
+
+            return -1;
         }
 
         // Definitions
@@ -72,6 +123,6 @@ namespace Riders.Tweakbox
         }
 
         [Function(CallingConventions.Stdcall)]
-        public unsafe delegate int LdrLoadDll(int searchPath, uint flags, Native.UNICODE_STRING* moduleFileName, out IntPtr handle);
+        public unsafe delegate uint LdrLoadDll(int searchPath, uint flags, Native.UNICODE_STRING* moduleFileName, out IntPtr handle);
     }
 }
