1bad498297d73a9dd5eb38a66ec6355d682f55b1
Sewer56 <seweratwork@gmail.com>
2021-05-31 16:55:31 +0100

    Rename Extension Class File Names

--- Riders.Tweakbox/Misc/Extensions/PointerExtensions.cs
+++ Riders.Tweakbox/Misc/Extensions/PointerExtensions.cs
@@ -1,6 +1,6 @@
 ﻿namespace Riders.Tweakbox.Misc.Extensions
 {
-    public static class Pointers
+    public static class PointerExtensions
     {
         public static unsafe bool IsNotNull<T>(T* ptr) where T : unmanaged => ptr != (void*) 0x0;
     }
