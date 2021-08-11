9aa8c0c4dfcba7d6aab841ca4ff0abcd2a560d1d
Sewer 56 <seweratwork@gmail.com>
2021-08-11 01:22:47 +0100

    Changed: Clean Up Tweakbox Settings Menu

--- Riders.Tweakbox/Components/Tweaks/TweakboxSettings.cs
+++ Riders.Tweakbox/Components/Tweaks/TweakboxSettings.cs
@@ -37,125 +37,164 @@ namespace Riders.Tweakbox.Components.Tweaks
             if (ImGui.Begin(Name, ref IsEnabled(), 0))
             {
                 ProfileSelector.Render();
-                EditFixes();
+                RenderMenu();
             }
 
             ImGui.End();
         }
 
-        private unsafe void EditFixes()
+        private unsafe void RenderMenu()
         {
             // Fix item width for long labels.
             ImGui.PushItemWidth(ImGui.GetFontSize() * - 12);
             var data = Config.Data;
 
             if (ImGui.CollapsingHeaderTreeNodeFlags("Graphics", 0))
-            {
-                ImGui.Text("Startup Settings");
-                if (RenderChangeResolutionCombo())
-                {
-                    var mode = _currentMode.Value;
-                    data.ResolutionX = mode.Width;
-                    data.ResolutionY = mode.Height;
-                    data.RaisePropertyUpdated(nameof(data.ResolutionX));
-                    data.RaisePropertyUpdated(nameof(data.ResolutionY));
-                }
-
-                if (!data.Fullscreen)
-                {
-                    ImGui.DragInt("Resolution X", ref data.ResolutionX, 1, 640, 16384, null, 0).Notify(data, nameof(data.ResolutionX));
-                    ImGui.DragInt("Resolution Y", ref data.ResolutionY, 1, 480, 16384, null, 0).Notify(data, nameof(data.ResolutionY));
-                }
-
-                if (IsFullscreenSupported())
-                    Reflection.MakeControl(ref data.Fullscreen, "Fullscreen").Notify(data, nameof(data.Fullscreen));
-
-                Reflection.MakeControl(ref data.Borderless, "Borderless Windowed").Notify(data, nameof(data.Borderless));
-                Reflection.MakeControl(ref data.Blur, "Blur").Notify(data, nameof(data.Blur));;
-                Reflection.MakeControl(ref data.WidescreenHack, "Centered Widescreen Hack").Notify(data, nameof(data.WidescreenHack));
-                Tooltip.TextOnHover("Basic widescreen hack that centers the game content to the screen.\n" +
-                                    "Do not combine/use with other widescreen hacks.");
-
-                if (ImGui.CollapsingHeaderTreeNodeFlags("Rendering Optimizations", 0))
-                {
-                    ImGui.Text($"Current FPS: " + _pacingController.Fps.StatFPS);
-
-                    ImGui.PushItemWidth(ImGui.GetFontSize() * -20);
-                    ImGui.Checkbox("Fix D3D Device Flags", ref data.D3DDeviceFlags).Notify(data, nameof(data.D3DDeviceFlags));
-                    Tooltip.TextOnHover("Applies on boot.");
-
-                    ImGui.Checkbox("Disable VSync ", ref data.DisableVSync).Notify(data, nameof(data.DisableVSync));
-                    Tooltip.TextOnHover("Applies on boot.");
-
-                    ImGui.Checkbox("FPS Cap", ref data.FpsCap).Notify(data, nameof(data.FpsCap));
-                    ImGui.Checkbox("Frame Pacing Fix", ref data.FramePacing).Notify(data, nameof(data.FramePacing));
-                    Tooltip.TextOnHover("Replaces game's framerate limiter with a custom one. Eliminates stuttering. Makes times more consistent.");
-
-                    if (Config.Data.FramePacing)
-                    {
-                        ImGui.Checkbox("Lag Compensation", ref data.FramePacingSpeedup).Notify(data, nameof(data.FramePacingSpeedup));
-                        Tooltip.TextOnHover("Speeds up the game to compensate for lag.");
-
-                        if (data.FramePacingSpeedup)
-                            Reflection.MakeControl(ref data.MaxSpeedupTimeMillis, "Lag Compensation Max Amount (Milliseconds)").Notify(data, nameof(data.MaxSpeedupTimeMillis));
-
-                        ImGui.Text($"CPU Load {_pacingController.CpuUsage:00.00}%");
-                        ImGui.Text($"Windows Timer Granularity: {_pacingController.TimerGranularity}ms");
-                        Reflection.MakeControl(ref data.DisableYieldThreshold, "CPU Spin Disable Thread Yield Threshold");
-                        Tooltip.TextOnHover("Calls Sleep(0) while spinning when CPU usage is below this threshold.");
-                    }
-
-                    ImGui.PopItemWidth();
-                }
-            }
+                RenderGraphicsMenu(data);
 
             if (ImGui.CollapsingHeaderTreeNodeFlags("Startup", 0))
-            {
-                ImGui.Checkbox("Boot to Menu & Unlock All", ref data.BootToMenu).Notify(data, nameof(data.BootToMenu));
-                if (data.BootToMenu && ImGui.TreeNodeStr("Boot to Menu Settings"))
-                {
-                    ImGui.Checkbox("Boot to Race", ref data.BootToRace).Notify(data, nameof(data.BootToRace));
-
-                    if (data.BootToRace && ImGui.TreeNodeStr("Boot to Race Settings"))
-                    {
-                        Reflection.MakeControlEnum(ref data.BootToRaceLevel, "Stage");
-                        Reflection.MakeControlEnum(ref data.BootToRaceCharacter, "Character");
-                        Reflection.MakeControlEnum(ref data.BootToRaceGear, "Gear");
-                        ImGui.TreePop();
-                    }
-
-                    ImGui.TreePop();
-                }
-
-                Reflection.MakeControlEnum(ref data.MemoryLimit, "Memory Limit (MB)");
-                Tooltip.TextOnHover("Sets the max amount of memory allowed to be used by the game's internal buffer.\n" +
-                                    "This value affects maximum file sizes allowed for native game models, textures etc.");
-            }
+                RenderStartupMenu(data);
 
             if (ImGui.CollapsingHeaderTreeNodeFlags("Misc", 0))
-            {
-                ImGui.Checkbox("Return to Stage Select from Race", ref data.NormalRaceReturnToTrackSelect).Notify(data, nameof(data.NormalRaceReturnToTrackSelect));
-                ImGui.Checkbox("Return to Stage Select from Tag", ref data.TagReturnToTrackSelect).Notify(data, nameof(data.TagReturnToTrackSelect));
-                ImGui.Checkbox("Return to Stage Select from Survival", ref data.SurvivalReturnToTrackSelect).Notify(data, nameof(data.SurvivalReturnToTrackSelect));
+                RenderMiscMenu(data);
 
-                ImGui.Checkbox("Automatic QTE Bug (Simulate Keyboard Left+Right Hold)", ref data.AutoQTE).Notify(data, nameof(data.AutoQTE));
+            // Restore item width
+            ImGui.PopItemWidth();
+        }
+
+        private void RenderGraphicsMenu(TweakboxConfig.Internal data)
+        {
+            ImGui.Text("Startup Settings");
+            if (RenderChangeResolutionCombo())
+            {
+                var mode = _currentMode.Value;
+                data.ResolutionX = mode.Width;
+                data.ResolutionY = mode.Height;
+                data.RaisePropertyUpdated(nameof(data.ResolutionX));
+                data.RaisePropertyUpdated(nameof(data.ResolutionY));
+            }
+
+            if (!data.Fullscreen)
+            {
+                ImGui.DragInt("Resolution X", ref data.ResolutionX, 1, 640, 16384, null, 0).Notify(data, nameof(data.ResolutionX));
+                ImGui.DragInt("Resolution Y", ref data.ResolutionY, 1, 480, 16384, null, 0).Notify(data, nameof(data.ResolutionY));
+            }
+
+            if (IsFullscreenSupported())
+                Reflection.MakeControl(ref data.Fullscreen, "Fullscreen").Notify(data, nameof(data.Fullscreen));
+
+            Reflection.MakeControl(ref data.Borderless, "Borderless Windowed").Notify(data, nameof(data.Borderless));
+            Reflection.MakeControl(ref data.Blur, "Blur").Notify(data, nameof(data.Blur));
+
+            if (ImGui.TreeNodeStr("High Quality Models"))
+            {
                 ImGui.Checkbox("Force Single Player Stage Data", ref data.SinglePlayerStageData).Notify(data, nameof(data.SinglePlayerStageData));
                 Tooltip.TextOnHover("Forces the game to load Single Player stage assets and Single Player Object Layout.");
 
                 ImGui.Checkbox("Force Single Player Models", ref data.SinglePlayerModels).Notify(data, nameof(data.SinglePlayerModels));
                 Tooltip.TextOnHover("Forces the game to load high quality single player models for all characters.");
-
-                ImGui.Checkbox("Include Vanilla Music", ref data.IncludeVanillaMusic).Notify(data, nameof(data.IncludeVanillaMusic));
-                Tooltip.TextOnHover("When playing custom music tracks, allow Vanilla tracks to still be played.");
-
-                ImGui.Checkbox("Allow going backwards in Races", ref data.AllowBackwardsDriving).Notify(data, nameof(data.AllowBackwardsDriving));
-
-                Reflection.MakeControlEnum(ref data.Language, "Message Language").Notify(data, nameof(data.Language));
-                Reflection.MakeControlEnum(ref data.VoiceLanguage, "Voice Language").Notify(data, nameof(data.VoiceLanguage));
+                ImGui.TreePop();
             }
 
-            // Restore item width
-            ImGui.PopItemWidth();
+            if (ImGui.TreeNodeStr("Rendering Optimizations"))
+            {
+                ImGui.Text($"Current FPS: " + _pacingController.Fps.StatFPS);
+
+                ImGui.PushItemWidth(ImGui.GetFontSize() * -20);
+                ImGui.Checkbox("Fix D3D Device Flags", ref data.D3DDeviceFlags).Notify(data, nameof(data.D3DDeviceFlags));
+                Tooltip.TextOnHover("Applies on boot.");
+
+                ImGui.Checkbox("Disable VSync", ref data.DisableVSync).Notify(data, nameof(data.DisableVSync));
+                Tooltip.TextOnHover("Applies on boot.");
+
+                ImGui.Checkbox("Remove FPS Cap", ref data.FpsCap).Notify(data, nameof(data.FpsCap));
+                Tooltip.TextOnHover("Intended for testing only.");
+
+                ImGui.Checkbox("Frame Pacing Fix", ref data.FramePacing).Notify(data, nameof(data.FramePacing));
+                Tooltip.TextOnHover("Replaces game's framerate limiter with a custom one. Eliminates stuttering. Makes times more consistent.");
+
+                if (Config.Data.FramePacing && ImGui.TreeNodeStr("Frame Pacing Settings"))
+                {
+                    ImGui.Checkbox("Lag Compensation", ref data.FramePacingSpeedup).Notify(data, nameof(data.FramePacingSpeedup));
+                    Tooltip.TextOnHover("Speeds up the game to compensate for lag.");
+
+                    if (data.FramePacingSpeedup)
+                        Reflection.MakeControl(ref data.MaxSpeedupTimeMillis, "Lag Compensation Max Amount (Milliseconds)").Notify(data, nameof(data.MaxSpeedupTimeMillis));
+
+                    ImGui.Text($"CPU Load {_pacingController.CpuUsage:00.00}%");
+                    ImGui.Text($"Windows Timer Granularity: {_pacingController.TimerGranularity}ms");
+                    Reflection.MakeControl(ref data.DisableYieldThreshold, "CPU Spin Disable Thread Yield Threshold");
+                    Tooltip.TextOnHover("Calls Sleep(0) while spinning when CPU usage is below this threshold.");
+                    ImGui.TreePop();
+                }
+
+                ImGui.PopItemWidth();
+                ImGui.TreePop();
+            }
+
+            if (ImGui.TreeNodeStr("Experimental"))
+            {
+                Reflection.MakeControl(ref data.WidescreenHack, "Centered Widescreen Hack").Notify(data, nameof(data.WidescreenHack));
+                Tooltip.TextOnHover("Basic widescreen hack that centers the game content to the screen.\n" +
+                                    "Do not combine/use with other widescreen hacks.");
+                ImGui.TreePop();
+            }
+        }
+
+        private static void RenderMiscMenu(TweakboxConfig.Internal data)
+        {
+            if (ImGui.TreeNodeStr("Main Menu Behaviour"))
+            {
+                ImGui.Checkbox("Return to Stage Select from Race", ref data.NormalRaceReturnToTrackSelect).Notify(data, nameof(data.NormalRaceReturnToTrackSelect));
+                ImGui.Checkbox("Return to Stage Select from Tag", ref data.TagReturnToTrackSelect).Notify(data, nameof(data.TagReturnToTrackSelect));
+                ImGui.Checkbox("Return to Stage Select from Survival", ref data.SurvivalReturnToTrackSelect).Notify(data, nameof(data.SurvivalReturnToTrackSelect));
+                ImGui.TreePop();
+            }
+
+            if (ImGui.TreeNodeStr("Race Tweaks"))
+            {
+                ImGui.Checkbox("Automatic QTE Bug (Simulate Keyboard Left+Right Hold)", ref data.AutoQTE).Notify(data, nameof(data.AutoQTE));
+                ImGui.Checkbox("Allow going backwards in Races", ref data.AllowBackwardsDriving).Notify(data, nameof(data.AllowBackwardsDriving));
+                ImGui.TreePop();
+            }
+
+            if (ImGui.TreeNodeStr("Music Injection"))
+            {
+                ImGui.Checkbox("Include Vanilla Music", ref data.IncludeVanillaMusic).Notify(data, nameof(data.IncludeVanillaMusic));
+                Tooltip.TextOnHover("When playing custom music tracks, allow Vanilla tracks to still be played.");
+                ImGui.TreePop();
+            }
+
+            if (ImGui.TreeNodeStr("Language Options"))
+            {
+                Reflection.MakeControlEnum(ref data.Language, "Message Language").Notify(data, nameof(data.Language));
+                Reflection.MakeControlEnum(ref data.VoiceLanguage, "Voice Language").Notify(data, nameof(data.VoiceLanguage));
+                ImGui.TextWrapped("Language changes will take effect after the next loading screen.");
+                ImGui.TreePop();
+            }
+        }
+
+        private static void RenderStartupMenu(TweakboxConfig.Internal data)
+        {
+            ImGui.Checkbox("Boot to Menu & Unlock All", ref data.BootToMenu).Notify(data, nameof(data.BootToMenu));
+            if (data.BootToMenu && ImGui.TreeNodeStr("Boot to Menu Settings"))
+            {
+                ImGui.Checkbox("Boot to Race", ref data.BootToRace).Notify(data, nameof(data.BootToRace));
+
+                if (data.BootToRace && ImGui.TreeNodeStr("Boot to Race Settings"))
+                {
+                    Reflection.MakeControlEnum(ref data.BootToRaceLevel, "Stage");
+                    Reflection.MakeControlEnum(ref data.BootToRaceCharacter, "Character");
+                    Reflection.MakeControlEnum(ref data.BootToRaceGear, "Gear");
+                    ImGui.TreePop();
+                }
+
+                ImGui.TreePop();
+            }
+
+            Reflection.MakeControlEnum(ref data.MemoryLimit, "Memory Limit (MB)");
+            Tooltip.TextOnHover("Sets the max amount of memory allowed to be used by the game's internal buffer.\n" +
+                                "This value affects maximum file sizes allowed for native game models, textures etc.");
         }
 
         private bool IsFullscreenSupported()
