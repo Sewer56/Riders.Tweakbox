94b0b74a6e3c05167792496e3968d49579905cfb
Sewer 56 <seweratwork@gmail.com>
2021-09-06 22:23:59 +0100

    Added: Startup Restriction Killing

---
+++ Riders.Tweakbox/Controllers/StartupRestrictionKill.cs
@@ -0,0 +1,21 @@
+﻿using Riders.Tweakbox.Controllers.Interfaces;
+using System;
+using Reloaded.Memory.Sources;
+
+namespace Riders.Tweakbox.Controllers
+{
+    public class StartupRestrictionKill : IController
+    {
+        private static byte[] _jmpSkipLauncher = { 0xEB, 0x23 }; // jmp 0x25
+        private static byte[] _jmpSkipOneInstance = { 0xEB, 0x28 }; // jmp 0x2A
+
+        public StartupRestrictionKill()
+        {
+            // Ignore launcher check result.
+            Memory.CurrentProcess.SafeWriteRaw((IntPtr)0x005118CF, _jmpSkipLauncher);
+
+            // Ignore only one instance check result.
+            Memory.CurrentProcess.SafeWriteRaw((IntPtr)0x0051190F, _jmpSkipOneInstance);
+        }
+    }
+}
