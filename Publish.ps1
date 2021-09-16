7b74c1473dd3388f9669570a5c77981767ea4d0a
Sewer 56 <seweratwork@gmail.com>
2021-09-16 23:28:32 +0100

    Cleanup: Publish Script & Mismatching folder Names

--- Publish.ps1
+++ Publish.ps1
@@ -1,36 +1,52 @@
 # Project Output Paths
-$modOutputPath = "TempBuild"
-$solutionName = "Riders.Tweakbox/Riders.Tweakbox.csproj"
-$publishName = "Riders.Tweakbox.zip"
 $publishDirectory = "Publish"
+$dir = Split-Path $MyInvocation.MyCommand.Path
+
+Write-host "My directory is $dir"
+Push-Location $dir
 
 [Environment]::CurrentDirectory = $PWD
 
+function Build
+{
+    param($BuildModOutputPath, $BuildCsProjPath, $BuildPublishName)
+
+    # Cleaup for Mod Build
+    Remove-Item $BuildModOutputPath -Recurse
+    New-Item $BuildModOutputPath -ItemType Directory
+
+    # Build
+    dotnet restore $BuildCsProjPath
+    dotnet clean $BuildCsProjPath
+    dotnet publish $BuildCsProjPath -c Release -r win-x86 --self-contained false -o "$BuildModOutputPath/x86" /p:PublishReadyToRun=false
+    
+    # Remove Redundant Files
+    Move-Item -Path "$BuildModOutputPath/x86/Tweakbox" -Destination "$BuildModOutputPath/Tweakbox"
+    Move-Item -Path "$BuildModOutputPath/x86/Preview.png" -Destination "$BuildModOutputPath/Preview.png"
+    Move-Item -Path "$BuildModOutputPath/x86/ModConfig.json" -Destination "$BuildModOutputPath/ModConfig.json"
+    Move-Item -Path "$BuildModOutputPath/x86/ReloadedGithubUpdater.json" -Destination "$BuildModOutputPath"
+    Move-Item -Path "$BuildModOutputPath/x86/Assets" -Destination "$BuildModOutputPath"
+
+    # Cleanup Unnecessary Files
+    Get-ChildItem $BuildModOutputPath -Include *.exe -Recurse | Remove-Item -Force -Recurse
+    Get-ChildItem $BuildModOutputPath -Include *.pdb -Recurse | Remove-Item -Force -Recurse
+    Get-ChildItem $BuildModOutputPath -Include *.xml -Recurse | Remove-Item -Force -Recurse
+
+    # Compress
+    Add-Type -A System.IO.Compression.FileSystem
+    [IO.Compression.ZipFile]::CreateFromDirectory($BuildModOutputPath, "$publishDirectory/$BuildPublishName")
+
+    # Cleanup After Build
+    Remove-Item $BuildModOutputPath -Recurse
+}
+
 # Clean anything in existing Release directory.
-Remove-Item $modOutputPath -Recurse
 Remove-Item $publishDirectory -Recurse
-New-Item $modOutputPath -ItemType Directory
 New-Item $publishDirectory -ItemType Directory
 
-# Build
-dotnet restore $solutionName
-dotnet clean $solutionName
-dotnet publish $solutionName -c Release -r win-x86 --self-contained false -o "$modOutputPath/x86" /p:PublishReadyToRun=false
+# Build Mods
+Build "TempBuild" "Riders.Tweakbox/Riders.Tweakbox.csproj" "Riders.Tweakbox.zip"
+Build "TempBuild" "Riders.Tweakbox.CharacterPack.DX/Riders.Tweakbox.CharacterPack.DX.csproj" "Riders.Tweakbox.CharacterPack.DX.zip"
+Build "TempBuild" "Riders.Tweakbox.Gearpack/Riders.Tweakbox.Gearpack.csproj" "Riders.Tweakbox.Gearpack.zip"
 
-# Remove Redundant Files
-Move-Item -Path "$modOutputPath/x86/Preview.png" -Destination "$modOutputPath/Preview.png"
-Move-Item -Path "$modOutputPath/x86/ModConfig.json" -Destination "$modOutputPath/ModConfig.json"
-Move-Item -Path "$modOutputPath/x86/ReloadedGithubUpdater.json" -Destination "$modOutputPath"
-Move-Item -Path "$modOutputPath/x86/Assets" -Destination "$modOutputPath"
-
-# Cleanup Unnecessary Files
-Get-ChildItem $modOutputPath -Include *.exe -Recurse | Remove-Item -Force -Recurse
-Get-ChildItem $modOutputPath -Include *.pdb -Recurse | Remove-Item -Force -Recurse
-Get-ChildItem $modOutputPath -Include *.xml -Recurse | Remove-Item -Force -Recurse
-
-# Compress
-Add-Type -A System.IO.Compression.FileSystem
-[IO.Compression.ZipFile]::CreateFromDirectory($modOutputPath, "$publishDirectory/$publishName")
-
-# Cleanup After Build
-Remove-Item $modOutputPath -Recurse
+Pop-Location
