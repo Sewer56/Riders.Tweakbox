91fc34131e7898fa0cd2a0e4f1695f75080e6286
Sewer 56 <seweratwork@gmail.com>
2021-09-18 21:47:55 +0100

    Corrected: DX Base Type Stats

--- Riders.Tweakbox.CharacterPack.DX/CustomCharacterPack.cs
+++ Riders.Tweakbox.CharacterPack.DX/CustomCharacterPack.cs
@@ -35,16 +35,8 @@ public class CustomCharacterPack
     public void SetCharacterStats()
     {
         var stats = Player.TypeStats;
-        stats[0].LevelOne.AdditiveSpeed = 0.777778f;
-        stats[0].LevelTwo.AdditiveSpeed = 0.810185f;
-        stats[0].LevelThree.AdditiveSpeed = 0.861111f;
-
-        stats[1].LevelOne.AdditiveSpeed = 0.745371f;
-        stats[1].LevelTwo.AdditiveSpeed = 0.777778f;
-        stats[1].LevelThree.AdditiveSpeed = 0.763889f;
-
-        stats[2].LevelOne.AdditiveSpeed = 0.759259f;
-        stats[2].LevelTwo.AdditiveSpeed = 0.791667f;
-        stats[2].LevelThree.AdditiveSpeed = 0.810186f;
+        stats[0].LevelTwo.AdditiveSpeed = 0.791666687f;
+        stats[1].LevelTwo.AdditiveSpeed = 0.777777791f;
+        stats[2].LevelTwo.AdditiveSpeed = 0.791666687f;
     }
 }
