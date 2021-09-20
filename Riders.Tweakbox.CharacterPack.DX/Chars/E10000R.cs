6c7f12743b10c4fa42d1a21ccb106ad71e6f3853
Sewer 56 <seweratwork@gmail.com>
2021-09-20 08:58:15 +0100

    Added: E10R as Power Type in DX Chars

--- Riders.Tweakbox.CharacterPack.DX/Chars/E10000R.cs
+++ Riders.Tweakbox.CharacterPack.DX/Chars/E10000R.cs
@@ -1,6 +1,8 @@
 ﻿using Sewer56.SonicRiders.Structures.Enums;
 using Riders.Tweakbox.CharacterPack.DX.Chars.Common;
 using Riders.Tweakbox.Interfaces.Interfaces;
+using Riders.Tweakbox.Interfaces.Structs.Characters;
+using CharacterType = Riders.Tweakbox.Interfaces.Structs.Characters.CharacterType;
 
 namespace Riders.Tweakbox.CharacterPack.DX.Chars;
 
@@ -9,4 +11,10 @@ public class E10000R : DriftCharacter, ICustomCharacter
     public override string Name { get; } = "E10000R DX";
 
     public override Characters Character { get; } = Characters.E10000R;
+
+    public ApiCharacterParameters GetCharacterParameters() => new ApiCharacterParameters()
+    {
+        SpeedMultiplierOffset = 0f,
+        ShortcutType = CharacterType.Power
+    };
 }
